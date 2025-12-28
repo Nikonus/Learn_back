0bea9cfcf5a3530d45d67358833b7fcab4a9781a
Nikhil dubey <nikhildubay.2005@gmail.com>
2025-12-28 14:21:38 +0530

    Create todo model with schema and timestamps

---
+++ todo.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+const todoSchema = new mongoose.Schema({
+  content:{
+    type:String,
+    required:true,
+
+  },
+  complete:{
+    type:Boolean,
+    default:false
+  },
+  createdBy:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User"
+  },
+  subtodos:[
+    {
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"sub_todo"
+    }
+  ]
+}, { timestamps: true });
+export const todo = mongoose.model('todo', todoSchema);
